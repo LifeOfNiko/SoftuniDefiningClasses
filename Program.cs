a0f2c1b7025479376b43c0144773d32c624602cb
lifeofniko <nikolaypetouhoff@gmail.com>
2023-10-03 12:20:17 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SoftuniDefiningClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
